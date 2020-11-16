fd8ab54dc4c33496e6798679190df15499a80ad9
TulebaevTemirlan <47266010+TulebaevTemirlan@users.noreply.github.com>
2020-11-16 13:56:59 +0600

    Anagrams or not

--- Task4/135.py
+++ Task4/135.py
@@ -1,18 +1,23 @@
 s1 = str(input("Enter first word - "))
 s2 = str(input("Enter second word - "))
 dic1 = {}
+dic2 = {}
+s1 = sorted(s1)
+s2 = sorted(s2)
 cnt = 0
 if(len(s1) != len(s2)):
     print("They can't be anagrams of each other, lengthes not equal")
 else:
     for i in range(0, len(s1)):
         dic1.update({i : s1[i]})
-    for y in range(0, len(s2)):
-        if(s2[y] not in dic1.values()):
+    for i in range(0, len(s2)):
+        dic2.update({i : s2[i]})
+    for key in dic1:
+        if(dic1[key] != dic2[key]):
             print("They can't be anagrams of each other")
             break
         else:
             cnt += 1
 if(cnt == len(s1)):
     print("They are anagrams of each other")
-
+    
---
+++ Task4/136.py
@@ -0,0 +1,31 @@
+s1 = str(input()).lower()
+s2 = str(input()).lower()
+s1 = s1.replace(" ", "")
+s2 = s2.replace(" ", "")
+s1 = sorted(s1)
+s2 = sorted(s2)
+iterator = 0
+cnt = 0
+ignored_chars = [" ", ",", ".", "!", ":", ";", "'", "?", "/"]
+dic1 = {}
+dic2 = {}
+for i in range(0, len(s1)):
+    if(s1[i] not in ignored_chars):
+        dic1.update({iterator: s1[i]})
+        iterator += 1
+iterator = 0
+for i in range(0, len(s2)):
+    if(s2[i] not in ignored_chars):
+        dic2.update({iterator: s2[i]})
+        iterator += 1
+if(len(dic1) != len(dic2)):
+    print("They can't be anagrams of each other, lengthes not equal")
+else:
+    for key in dic1:
+        if(dic1[key] != dic2[key]):
+            print("They can't be anagrams of each other")
+            break
+        else:
+            cnt += 1
+if(cnt == len(dic1)):
+    print("They are anagrams of each other")
